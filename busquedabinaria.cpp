af57cf9a14bc5af96c24fb2a85f654752143bb8e
Paperkoops <paperkoops@gmail.com>
2019-09-03 19:03:36 -0600

    Agregado el ejercicio de busqueda binaria

---
+++ busquedabinaria.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+int buscar(int low, int high, int num);
+int a[] = {1,3,4,5,17,18,31,33};
+int main()
+{   
+int num;
+cin >> num;
+
+cout << buscar(0, 7, num);
+}
+
+int buscar (int low, int high, int num){
+	int mid;
+	if (low>high) return (-1);
+	mid = (low + high) / 2;
+	if (num == a[mid]) return (mid);
+	if (num < a[mid]) return (buscar(low, mid-1, num));
+	else return (buscar(mid+1, high, num));
+	
+}
