bf7b1dadd9db43d8f477e325d52f6b231d9c8c81
Paperkoops <paperkoops@gmail.com>
2019-09-06 20:32:31 -0600

    El tamaño de los pangolines varia según las especies, desde 30 cm hasta 1 metro. Las hembras son generalmente más pequeñas. La apariencia externa de los pangolines la caracterizan las escamas largas, endurecidas y en forma de placas que recubren su cuerpo, lo cual es un atributo inusual entre los mamíferos. Son a menudo comparados con alcachofas o piñas caminantes. Se pueden enrollar en una bola cuando se ven amenazados, con sus escamas sobrepuestas actuando como armadura.1​ Al nacer, las escamas son blandas, pero se van endureciendo a medida que el animal se desarrolla.

--- Lab 1/Ejercicio8.cpp
+++ Lab 1/Ejercicio8.cpp
@@ -1,44 +1,28 @@
-
 #include <iostream>
 
 using namespace std;
-void contar(int direc, int num, int act);
-
+void imprimirConteo(int num, int act);
 int main()
 {
-    int x;
-    cin >> x;
-    contar(1, x, 1);
-
-    return 0;
+   int num = 0;
+   cout<<"Ingrese un numero: "<<endl;
+   cin>> num;
+   
+   imprimirConteo(num, 0);
+   
+   return 0;
 }
-
-void contar(int direc, int num, int act)
-{
-    if(num == act)
+void imprimirConteo(int num, int act){
+    
+    act++;
+    if(num==act)
     {
-        cout << act << endl;
-        contar(0, 1, act);
+        cout<<act;
     }
-    else if(direc == 1)
+    else
     {
-        cout << act << endl;
-        contar(direc, num, act+1);
-        cout << "oh no" << endl;
-    }
-    else if(direc == 0){
-        cout << act << endl;
-        
-        if(act == 1)
-        {
-            //el ciclo termina
-        }
-        else
-        {
-            contar(direc, num, act-1);
-        }
-        //restas 1 al actual y volver a llamar
+        cout<<act;
+        imprimirConteo(num,act);
+        cout<<act;
     }
 }
-
-
